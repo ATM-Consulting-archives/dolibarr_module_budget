68772fd46b4814f94b2404d58d171496b2512b07
arnaud <arnaud.saint-patrice@atm-consulting.fr>
2016-04-12 17:39:45 +0200

    INIT module budget

--- class/budget.class.php
+++ class/budget.class.php
@@ -9,10 +9,11 @@ class TBudget extends TObjetStd {
 		global $langs;
 		
         parent::set_table(MAIN_DB_PREFIX.'sig_budget');
-		parent::add_champs('fk_type_object','type=chaine;');
-		parent::add_champs('fk_object','type=integer;');
+		parent::add_champs('date_debut, date_fin',array('type'=>'date', 'index'=>true));
+		parent::add_champs('fk_project',array('type'=>'integer', 'index'=>true));
+		parent::add_champs('statut',array('type'=>'integer'));
 		
-		parent::_init_vars();
+		parent::_init_vars('label');
         parent::start();
 
 	}
@@ -29,13 +30,11 @@ class TBudgetLine extends TObjetStd {
 		global $langs;
 		
         parent::set_table(MAIN_DB_PREFIX.'sig_budget_ligne');
-		parent::add_champs('label','type=chaine;');
+		parent::add_champs('code_compta','type=chaine;');
 		parent::add_champs('price','type=integer;');
 		
 		parent::_init_vars();
         parent::start();
 
 	}
-	
-	
 }
