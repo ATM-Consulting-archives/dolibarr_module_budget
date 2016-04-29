2b5fc1918cf4ae40a4d154cab6a7add27035f891
arnaud <arnaud.saint-patrice@atm-consulting.fr>
2016-04-29 17:16:48 +0200

    NEW droit encours et fix

--- core/modules/modbudget.class.php
+++ core/modules/modbudget.class.php
@@ -200,6 +200,13 @@ class modbudget extends DolibarrModules
 		$this->rights[$r][6] = 'read';
 		$r++;
 
+		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
+		$this->rights[$r][1] = 'Modifier les encours';	// Permission label
+		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
+		$this->rights[$r][4] = 'encours';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
+		$this->rights[$r][5] = 'edit';
+		$r++;
+
 		// Main menu entries
 		$this->menu = array();			// List of menus to add
 		$r=0;
--- script/interface.php
+++ script/interface.php
@@ -1,3 +1,60 @@
 <?php
-	echo 'Hellow world!';
+require '../config.php';
+
+dol_include_once("/budget/class/budget.class.php");
+dol_include_once("/budget/class/encours.class.php");
+dol_include_once('/sig/class/categorie_comptable.class.php');
+
+if(empty($conf->sig->enabled)) exit('SIGrequire');
+
+// Contrôle d'accès
+if (!($user->admin || $user->rights->budget->read)) {
+    accessforbidden();
+}
+
+$langs->load('budget@budget');
+
+$PDOdb=new TPDOdb;
+
+// Get parameters
+_action($PDOdb);
+
+function _action(&$PDOdb) {
+	global $user, $conf,$langs;
+	
+	$encours = new TEncours;
+	$action = GETPOST('action');
+	
+	switch($action) {
+		
+		case 'update_encours':
+			$identifiant=GETPOST('identifiant');
+			$type_object = GETPOST('type_object');
+			$month = (int) GETPOST('month');
+			$year = (int) GETPOST('year');
+			$price = GETPOST('price');
+			
+			$encours->load_for_date($PDOdb, $identifiant, $type_object, $year, $month);
+			
+			$date_encours = strtotime(date($year.'-'.$month.'-01'));
+			$encours->fk_object = $identifiant;
+			$encours->type_object = $type_object;
+			$encours->price = $price;
+			$encours->date_encours = $date_encours;
+			$encours->save($PDOdb);
+			break;
+		case 'delete_encours':
+			$identifiant=GETPOST('identifiant');
+			$type_object = GETPOST('type_object');
+			$month = (int) GETPOST('month');
+			$year = (int) GETPOST('year');
+			$price = GETPOST('price');
+			
+			$encours->load_for_date($PDOdb, $identifiant, $type_object, $year, $month);
+			
+			$encours->delete($PDOdb);
+			break;
+	}
+}
+			
 ?>
